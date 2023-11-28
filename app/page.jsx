aee87da6016527e9f367c269d30ac8e214f838ff
dongha0212 <dongha020212@gmail.com>
2023-11-28 11:34:43 +0900

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import TopicsList from '@/components/TopicsList'
+export default function Home() {
+  return (
+    <>
+            <h1 className="text-3xl font-bold">WebDev Topics</h1> 
+      <p className="mb-4">MongoDB CRUD Example</p>
+    </>
+  )
+}
