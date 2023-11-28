ab3dda31f350eaa017dcba45dccdd7c4401b9d8c
dongha0212 <dongha020212@gmail.com>
2023-11-28 11:47:36 +0900

    오타수정

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import TopicList from '@/components/TopicList';
+import TopicsList from '@/components/TopicsList';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Recent News</h1>
-      <TopicList />
+      <TopicsList />
     </div>
   );
 }
